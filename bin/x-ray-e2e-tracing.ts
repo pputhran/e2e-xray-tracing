bd85877d02917a19ab174aa85ac421e8539ebcff
Pawan Puthran <pputhran@amazon.com>
2020-06-08 17:29:25 +0100

    Tracing

--- bin/x-ray-e2e-tracing.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { XRayE2ETracingStack } from '../lib/x-ray-e2e-tracing-stack';
-
-const app = new cdk.App();
-new XRayE2ETracingStack(app, 'XRayE2ETracingStack');
